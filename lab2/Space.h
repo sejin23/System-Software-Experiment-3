8e309b4a809f2dbd103ecbdb5e346b0b924adce1
sejin <sejin4430@gmail.com>
2018-11-11 13:05:28 +0900

    file create

---
+++ lab2/Space.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class Space{
+    private:
+        
+    protected:
+
+};
+
+class Study_Room: public Space{
+    private:
+        int room_mem;
+        string reserve_mem;
+};
+
+class Seat: public Space{
+    private:
+        string seat_name;
+
+};
