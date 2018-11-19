b54d5c5b6f9bb09bb2d56ce1334fc9533571aadb
sejin <sejin4430@gmail.com>
2018-11-19 17:26:21 +0900

    resource file

--- lab2/Resource.cpp
+++ lab2/Resource.cpp
@@ -2,187 +2,75 @@
 #include <fstream>
 #include <string>
 #include "Resource.h"
+using namespace std;
 
-int CompareDate(Date* srcDate, Date* dueDate){
-	if(srcDate->year > dueDate->year) return 0;
-	else if(srcDate->month > dueDate->month) return 0;
-	else if(srcDate->day > dueDate->day) return 0;
-	return 1;
-}
-
-Book::Book(){
-	header = NULL;
-}
-
-Book::~Book(){
-	Rsrcs* temp = header;
-	Rsrcs* prev;
-	while(temp != NULL){
-		prev = temp->next;
-		if(temp->Mem != NULL) delete temp->Mem;
-		delete temp;
-		temp = prev;
-	}
-}
-
-void Book::setBook(string src){
-	Rsrcs* newSrc = new Rsrcs;
-	Rsrcs* temp = header;
-	newSrc->Rsrcname = src;
-	newSrc->next = NULL;
-	newSrc->state = 'N';
-	if(header == NULL) header = newSrc;
-	else{
-		while(temp->next != NULL) temp = temp->next;
-		temp->next = newSrc;
-	}
-}
-
-Date* Book::getBookdueDate(string src){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!src.compare(temp->Rsrcname)) return temp->dueDate;
-		temp = temp->next;
-	}
-}
-
-int Book::IsExisted(string src){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!src.compare(temp->Rsrcname)) return 1;
-		temp = temp->next;
-	}
-	return 0;
-}
-
-int Book::IsOverlap(string name, string src){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!src.compare(temp->Rsrcname) && temp->Mem != NULL){
-			if(!name.compare(temp->Mem->getMemName())) return 1;
-		}
-		temp = temp->next;
-	}
-	return 0;
-}
-
-int Book::getBorrowBook(string src, string memname, string memtype, Date* borrowdate){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!temp->Rsrcname.compare(src)){
-			if(temp->state == 'B'){
-				if(!memname.compare(temp->Mem->getMemName())) return 4;
-				else return 5;
-			}
-			return 0;
-		}
-		temp = temp->next;
+int CompareDate(int* srcDate, int* dueDate){
+	int i;
+	for(i=0;i<3;i++){
+		if(srcDate[i] > dueDate[i]) return 0;
 	}
 	return 1;
 }
 
-int Book::setBorrowBook(string src, string memname, string memtype, Date* borrowdate){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!temp->Rsrcname.compare(src)){
-			if(temp->state == 'B'){
-				if(!memname.compare(temp->Mem->getMemName())) return 4;
-				else return 5;
-			}
-			temp->Mem = new Member(memname, memtype);
-			temp->dueDate = new Date;
-			temp->dueDate->year = borrowdate->year + (borrowdate->month + (borrowdate->day + 13)/30)/12;
-			temp->dueDate->month = (borrowdate->month + (borrowdate->day + 13)/30)%12;
-			temp->dueDate->day = (borrowdate->day + 13)%30;
-			temp->state = 'B';
-			return 0;
-		}
-		temp = temp->next;
-	}
-	return 1;
-}
-
-int Book::setReturnBook(string src, string memname, string memtype, Date* returndate){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!temp->Rsrcname.compare(src)){
-			if(temp->state == 'B' && CompareDate(returndate, temp->dueDate)){
-				if(memname.compare(temp->Mem->getMemName())) return 3;
-				temp->state = 'N';
-				delete temp->Mem;
-				temp->Mem = NULL;
-				return 0;
-			}else if(temp->state == 'N') return 3;
-			else{
-				temp->state = 'N';
-				delete temp->Mem;
-				temp->Mem = NULL;
-				return 7;
-			}
-		}
-		temp = temp->next;
-	}
-	return 1;
-}
-
-int Book::getReturnBook(string src, string memname, string memtype, Date* returndate){
-	Rsrcs* temp = header;
-	while(temp != NULL){
-		if(!temp->Rsrcname.compare(src)){
-			if(temp->state == 'B' && CompareDate(returndate, temp->dueDate)){
-				if(memname.compare(temp->Mem->getMemName())) return 3;
-				return 0;
-			}else if(temp->state == 'N') return 3;
-			else return 7;
-		}
-		temp = temp->next;
-	}
-	return 1;
-}
-
-Resource::Resource(){
+Resource::Resource(string src_name){
+	int i;
+	srcname = src_name;
+	state = 'X';
+	und = NULL;
+	for(i=0;i<3;i++) duedate[i] = 0;
 }
 
 Resource::~Resource(){
+	delete und;
 }
 
-void Resource::setResrc(){
-	string rsrcType, rsrcName;
-	ifstream fin("resource.dat");
-	fin >> rsrcType >> rsrcName;
-	while(!fin.eof()){
-		fin >> rsrcType >> rsrcName;
-		if(!rsrcType.compare("Book")) Book::setBook(rsrcName);
-		rsrcType = "";
-		rsrcName = "";
+Book::Book(string src_name):Resource(src_name){
+
+}
+
+char Book::getrscstate(){
+	return state;
+}
+
+string Book::getsrcname(){
+	return srcname;
+}
+
+string Book::getusername(){
+	if(und != NULL) return und->getUserName();
+	else return "NULL";
+}
+
+void Book::borrowresrc(string mem_name, string mem_type, int* d){
+	state = 'B';
+	if(und != NULL) delete und;
+	und = new Undergraduate();
+	und->setmember(mem_name);
+	duedate[2] = d[2] + 13;
+	duedate[1] = d[1];
+	duedate[0] = d[0];
+	if(duedate[2] > 30){
+		duedate[2] -= 30;
+		duedate[1]++;
+	}
+	if(duedate[1] > 12){
+		duedate[1] -= 12;
+		duedate[0]++;
 	}
 }
 
-int Resource::findResrc(string src, string type){
-	if(!type.compare("Book"))
-		return Book::IsExisted(src);
+int Book::returnresrc(string mem_name, string mem_type, int* d){
+	state = 'X';
+	delete und;
+	und = NULL;
+	if(CompareDate(d, duedate))
+		return 1;
+	else return 2;
 }
 
-int Resource::findOverlap(string memname, string srcname, string srctype, char state){
-	if(state == 'R') return 0;
-	if(!srctype.compare("Book"))
-		return Book::IsOverlap(memname, srcname);
-}
-
-int Resource::searchResrc(string src, string type, string memname, string memtype, Date* strdate, char stat){
-	if(!type.compare("Book")){
-		if(stat == 'B') return Book::setBorrowBook(src, memname, memtype, strdate);
-		else if(stat == 'R') return Book::setReturnBook(src, memname, memtype, strdate);
-	}
-}
-
-int Resource::confirmResrc(string src, string type, string memname, string memtype, Date* strdate, char stat){
-	if(!type.compare("Book")){
-		if(stat == 'B') return Book::getBorrowBook(src, memname, memtype, strdate);
-		else if(stat == 'R') return Book::getReturnBook(src, memname, memtype, strdate);
-	}
-}
-
-Date* Resource::getdueDate(string src, string type){
-	return Book::getBookdueDate(src);
+int* Book::getdelay(){
+	int* d = new int[3];
+	int i;
+	for(i=0;i<3;i++) d[i] = duedate[i];
+	return d;
 }
--- lab2/Resource.h
+++ lab2/Resource.h
@@ -2,44 +2,25 @@
 #include "Member.h"
 using namespace std;
 
-typedef struct _date{
-	int year, month, day;
-}Date;
-
-typedef struct _rsrcs{
-	Member* Mem;
-	string Rsrcname;
-	struct _rsrcs* next;
-	Date* dueDate;
-	char state;
-}Rsrcs;
-
-class Book{
-	private:
-		Rsrcs* header;
+class Resource{
 	protected:
-		Book();
-		~Book();
-		void setBook(string src);
-		Date* getBookdueDate(string src);
-		int IsExisted(string src);
-		int IsOverlap(string name, string src);
-		int setBorrowBook(string src, string memname, string memtype, Date* borrowdate);
-		int getBorrowBook(string src, string memname, string memtype, Date* borrowdate);
-		int setReturnBook(string src, string memname, string memtype, Date* returndate);
-		int getReturnBook(string src, string memname, string memtype, Date* returndate);
-};
-
-class Resource: public Book{
-	public:
-		Resource();
+		string srcname;
+		char state;
+		int duedate[3];
+		Undergraduate* und;
+		Resource(string src_name);
 		~Resource();
-		void setResrc();
-		int findResrc(string src, string type);
-		int findOverlap(string memname, string srcname, string srctype, char state);
-		int searchResrc(string src, string type, string memname, string memtype, Date* strdate, char stat);
-		int confirmResrc(string src, string type, string memname, string memtype, Date* strdate, char stat);
-		Date* getdueDate(string src, string type);
 };
 
-int CompareDate(Date* srcDate, Date* dueDate);
+class Book: public Resource{
+	public:
+		Book(string src_name);
+		char getrscstate();
+		string getsrcname();
+		string getusername();
+		void borrowresrc(string mem_name, string mem_type, int* d);
+		int returnresrc(string mem_name, string mem_type, int* d);
+		int* getdelay();
+};
+
+int CompareDate(int* srcDate, int* dueDate);
