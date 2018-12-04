711a9f2ee439831f21596459ff5c6290a6713ec0
sejin <sejin4430@gmail.com>
2018-12-04 16:10:15 +0900

    main 수정

--- lab3/Library.cpp
+++ lab3/Library.cpp
@@ -11,6 +11,22 @@ int namewithnumbers(string name){
     return 0;
 }
 
+int cmpdate(int* in, int* sp){
+    if(in[0] != sp[0]%100){
+        if(in[0] > sp[0]%100) return 2;
+        else return 1;
+    }
+    if(in[1] != sp[1]){
+        if(in[1] > sp[1]) return 2;
+        else return 1;
+    }
+    if(in[2] != sp[2]){
+        if(in[2] > sp[2]) return 2;
+        else return 1;
+    }
+    return 1;
+}
+
 int mgzcmp(string src, int* date){
     string year = src.substr(1, 2), month = src.substr(4, 5);
     int yy, mm;
@@ -31,18 +47,24 @@ int mgzcmp(string src, int* date){
 }
 
 Library::Library(int fl, int room_n, int seat_n){
-    int i, j;
+    int i, j, k;
     string rsrcType, rsrcName, Name, cap;
+    string sp_type, mem_type, mem_name;
+    string rsc_type, rsc_name, inmem_type, inmem_name;
+    char op, inop, sl;
+    int sp_num, num_of_mem, rsv_time, inbreak, spbreak;
+    int* date = new int[4];
+    int* indate = new int[3];
     floor_m = fl;
     room_m = room_n;
     seat_m = seat_n;
-    ifstream fin("resource.dat");
+    ifstream fre("resource.dat");
     ofstream fout("output.dat");
     fout << "Op_#\tReturn_code\tDescription";
-    fin >> rsrcType >> rsrcName;
+    fre >> rsrcType >> rsrcName;
     while(1){
-        fin >> rsrcType >> rsrcName;
-        if(fin.eof()) break;
+        fre >> rsrcType >> rsrcName;
+        if(fre.eof()) break;
         if(!rsrcType.compare("Book")){
             Book temp(rsrcName);
             book.push_back(temp);
@@ -58,6 +80,7 @@ Library::Library(int fl, int room_n, int seat_n){
             ebook.push_back(temp);
         }
     }
+    fre.close();
     fout.close();
     str = new Study_Room*[room_n];
     for(i=0;i<room_n;i++) str[i] = new Study_Room();
@@ -69,6 +92,60 @@ Library::Library(int fl, int room_n, int seat_n){
     seat_status = new int[fl];
     for(i=0;i<fl;i++) seat_status[i] = 0;
     room_state = 0;
+    i = 1;
+    k = 0;
+    inbreak = 0;
+    spbreak = 0;
+    ifstream fin("input.dat");
+    ifstream fsp("space.dat");
+    getline(fin, sp_type);
+    getline(fsp, sp_type);
+    while(1){
+        if(k == 0){
+            fin >> indate[0] >> sl >> indate[1] >> sl >> indate[2];
+            fin >> rsc_type >> rsc_name >> inop >> inmem_type >> inmem_name;
+            fsp >> date[0] >> sl >> date[1] >> sl >> date[2] >> sl >> date[3];
+            fsp >> sp_type >> sp_num >> op >> mem_type >> mem_name;
+            if(op == 'B') fsp >> num_of_mem >> rsv_time;
+        }else if(k == 1){
+            fin >> indate[0] >> sl >> indate[1] >> sl >> indate[2];
+            if(fin.eof()) inbreak = 1;
+            fin >> rsc_type >> rsc_name >> inop >> inmem_type >> inmem_name;
+        }else{
+            fsp >> date[0] >> sl >> date[1] >> sl >> date[2] >> sl >> date[3];
+            if(fsp.eof()) spbreak = 1;
+            fsp >> sp_type >> sp_num >> op >> mem_type >> mem_name;
+            if(op == 'B') fsp >> num_of_mem >> rsv_time;
+        }
+        if(!inbreak && !spbreak)
+            k = cmpdate(indate, date);
+        else if(inbreak && spbreak)
+            break;
+        else{
+            if(inbreak) k = 2;
+            else if(spbreak) k = 1;
+        }
+        try{
+            if(k == 1) BorrowResource(i, indate, rsc_type, rsc_name, inmem_type, inmem_name, inop);
+            else if(k == 2) BorrowSpace(i, date, sp_type, sp_num, mem_type, mem_name, op, num_of_mem, rsv_time);
+        }catch(int err){
+            ofstream fout;
+            fout.open("output.dat", ios_base::out | ios_base::app);
+            fout << endl << i << "\t-1\t";
+            if(err == 1) fout << "Date out of range.";
+            else if(err == 2) fout << "Non-exist space type.";
+            else if(err == 3) fout << "Non-exist operation.";
+            else if(err == 4) fout << "Non-exist member type.";
+            else if(err == 5) fout << "Member name with numbers.";
+            else if(err == 6) fout << "Nagative.";
+            fout.close();
+        }
+        i++;
+    }
+    fin.close();
+    fsp.close();
+    delete[] indate;
+    delete[] date;
 }
 
 Library::~Library(){
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,82 +4,8 @@
 #include "Library.h"
 using namespace std;
 
-int cmpdate(int* in, int* sp){
-    if(in[0] != sp[0]%100){
-        if(in[0] > sp[0]%100) return 2;
-        else return 1;
-    }
-    if(in[1] != sp[1]){
-        if(in[1] > sp[1]) return 2;
-        else return 1;
-    }
-    if(in[2] != sp[2]){
-        if(in[2] > sp[2]) return 2;
-        else return 1;
-    }
-    return 1;
-}
-
 int main(int argc, char** argv){
-    string sp_type, mem_type, mem_name;
-    string rsc_type, rsc_name, inmem_type, inmem_name;
-    int sp_num, num_of_mem, rsv_time;
-    int i = 1, j, avt, k = 0, inbreak = 0, spbreak = 0;
-    char op, inop, sl;
-    int* date = new int[4];
-    int* indate = new int[3];
-    int* res;
     Library* lib;
-    ifstream fin("input.dat");
-    ifstream fsp("space.dat");
-    getline(fin, sp_type);
-    getline(fsp, sp_type);
     lib = new Library(3, 10, 50);
-    while(1){
-        if(k == 0){
-            fin >> indate[0] >> sl >> indate[1] >> sl >> indate[2];
-            fin >> rsc_type >> rsc_name >> inop >> inmem_type >> inmem_name;
-            fsp >> date[0] >> sl >> date[1] >> sl >> date[2] >> sl >> date[3];
-            fsp >> sp_type >> sp_num >> op >> mem_type >> mem_name;
-            if(op == 'B') fsp >> num_of_mem >> rsv_time;
-        }else if(k == 1){
-            fin >> indate[0] >> sl >> indate[1] >> sl >> indate[2];
-            if(fin.eof()) inbreak = 1;
-            fin >> rsc_type >> rsc_name >> inop >> inmem_type >> inmem_name;
-        }else{
-            fsp >> date[0] >> sl >> date[1] >> sl >> date[2] >> sl >> date[3];
-            if(fsp.eof()) spbreak = 1;
-            fsp >> sp_type >> sp_num >> op >> mem_type >> mem_name;
-            if(op == 'B') fsp >> num_of_mem >> rsv_time;
-        }
-        if(!inbreak && !spbreak)
-            k = cmpdate(indate, date);
-        else if(inbreak && spbreak)
-            break;
-        else{
-            if(inbreak) k = 2;
-            else if(spbreak) k = 1;
-        }
-        try{
-            if(k == 1) lib->BorrowResource(i, indate, rsc_type, rsc_name, inmem_type, inmem_name, inop);
-            else if(k == 2) lib->BorrowSpace(i, date, sp_type, sp_num, mem_type, mem_name, op, num_of_mem, rsv_time);
-        }catch(int err){
-            ofstream fout;
-            fout.open("output.dat", ios_base::out | ios_base::app);
-            fout << endl << i << "\t-1\t";
-            if(err == 1) fout << "Date out of range.";
-            else if(err == 2) fout << "Non-exist space type.";
-            else if(err == 3) fout << "Non-exist operation.";
-            else if(err == 4) fout << "Non-exist member type.";
-            else if(err == 5) fout << "Member name with numbers.";
-            else if(err == 6) fout << "Nagative.";
-            fout.close();
-        }
-        i++;
-    }
-    fin.close();
-    fsp.close();
     delete lib;
-    delete[] indate;
-    delete[] date;
 }
