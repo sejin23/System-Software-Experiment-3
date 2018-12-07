52f5ae7318d580d255f85b12d89e6f17b9460a8e
sejin <sejin4430@gmail.com>
2018-12-08 01:18:47 +0900

    shell file complete

--- lab3/library_statistics.sh
+++ lab3/library_statistics.sh
@@ -56,27 +56,231 @@ then
 elif [ $1 = 'input' ]
 then
 	mkdir -p "./result/input"
-
+	if [ $2 = 'all' ]
+	then
+		./library_statistics.sh input book
+		./library_statistics.sh input e-book
+		./library_statistics.sh input magazine
+		./library_statistics.sh input undergraduate
+		./library_statistics.sh input graduate
+		./library_statistics.sh space faculty
+	else
+		echo $'Date[yy/mm/dd/hh]\tSpace_type\tSpace_number\tOperation\tMember_type\tMember_name\tNumber_of_member' > ./stream/space.dat
+		echo $'Date[yy/mm/dd]\tResource_type\tResource_name\tOperation\tMember_type\tMember_name' > ./stream/input.dat
+		if [ $2 = 'book' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[1]}" == "Book" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/book.dat
+		elif [ $2 = 'magazine' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[1]}" == "Magazine" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/magazine.dat
+		elif [ $2 = 'e-book' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[1]}" == "E-book" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/e-book.dat
+		elif [ $2 = 'undergraduate' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Undergraduate" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/undergraduate.dat
+		elif [ $2 = 'graduate' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Graduate" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/graduate.dat
+		elif [ $2 = 'faculty' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Faculty" ]
+				then
+					echo ${dat[@]} >> ./stream/input.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/faculty.dat
+		elif [ $2 = 'date' ]
+		then
+			awk '{print}' ./input.dat | while read -a dat
+			do
+				if [ "${dat[0]}" != "Date[yy/mm/dd]" ]
+				then
+					year=`echo ${dat[0]} | cut -d "/" -f1`
+					month=`echo ${dat[0]} | cut -d "/" -f2`
+					day=`echo ${dat[0]} | cut -d "/" -f3`
+					fromy=`echo $3 | cut -d "/" -f1`
+					fromm=`echo $3 | cut -d "/" -f2`
+					fromd=`echo $3 | cut -d "/" -f3`
+					toy=`echo $4 | cut -d "/" -f1`
+					tom=`echo $4 | cut -d "/" -f2`
+					tod=`echo $4 | cut -d "/" -f3`
+					time=`expr $year \* 360 + $month \* 30 + $day`
+					fromt=`expr $fromy \* 360 + $fromm \* 30 + $fromd`
+					tot=`expr $toy \* 360 + $tom \* 30 + $tod`
+					if [ $time -ge $fromt -a $time -le $tot ]
+					then
+						echo ${dat[@]} >> ./stream/input.dat
+					fi
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/input/date.dat
+		fi
+		cp space.dat ./stream/space.dat
+		cp input.dat ./stream/input.dat
+	fi
 elif [ $1 = 'space' ]
 then
 	mkdir -p "./result/space"
-	echo $'Date[yy/mm/dd/hh]\tSpace_type\tSpace_number\tOperation\tMember_type\tMember_name\tNumber_of_member' > ./stream/space.dat
-	echo $'Date[yy/mm/dd]\tResource_type\tResource_name\tOperation\tMember_type\tMember_name' > ./stream/input.dat
-	if [ $2 = 'studyroom' ]
+	if [ $2 = 'all' ]
 	then
-		awk '{print}' ./space.dat | while read -a dat
-		do
-			if [ $3 = 'all' ]
-			then
-
-				if [ "${dat[1]}" == "StudyRoom" ]
+		./library_statistics.sh space studyroom all
+		./library_statistics.sh space seat all
+		./library_statistics.sh space undergraduate
+		./library_statistics.sh space graduate
+		./library_statistics.sh space faculty
+	else
+		echo $'Date[yy/mm/dd/hh]\tSpace_type\tSpace_number\tOperation\tMember_type\tMember_name\tNumber_of_member' > ./stream/space.dat
+		echo $'Date[yy/mm/dd]\tResource_type\tResource_name\tOperation\tMember_type\tMember_name' > ./stream/input.dat
+		if [ $2 = 'studyroom' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ $3 = 'all' ]
+				then
+					if [ "${dat[1]}" == "StudyRoom" ]
+					then
+						echo ${dat[@]} >> ./stream/space.dat
+					fi
+				elif [ $3 -le 10 -a $3 -gt 0 ]
+				then
+					if [ "${dat[1]}" == "StudyRoom" -a "$3" == "${dat[2]}" ]
+					then
+						echo ${dat[@]} >> ./stream/space.dat
+					fi
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/studyroom.dat
+		elif [ $2 = 'seat' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ $3 = 'all' ]
+				then
+					if [ "${dat[1]}" == "Seat" ]
+					then
+						echo ${dat[@]} >> ./stream/space.dat
+					fi
+				elif [ $3 -le 3 -a $3 -gt 0 ]
+				then
+					if [ "${dat[1]}" == "Seat" -a "$3" == "${dat[2]}" ]
+					then
+						echo ${dat[@]} >> ./stream/space.dat
+					fi
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/seat.dat
+		elif [ $2 = 'undergraduate' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Undergraduate" ]
 				then
 					echo ${dat[@]} >> ./stream/space.dat
 				fi
-			elif [ $3 -le 10 -a $3 -gt 0 ]
-		done
-	./lib go > ./result/space/studyroom.dat
-	
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/undergraduate.dat
+		elif [ $2 = 'graduate' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Graduate" ]
+				then
+					echo ${dat[@]} >> ./stream/space.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/graduate.dat
+		elif [ $2 = 'faculty' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ "${dat[4]}" == "Faculty" ]
+				then
+					echo ${dat[@]} >> ./stream/space.dat
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/faculty.dat
+		elif [ $2 = 'date' ]
+		then
+			awk '{print}' ./space.dat | while read -a dat
+			do
+				if [ "${dat[0]}" != "Date[yy/mm/dd/hh]" ]
+				then
+					year=`echo ${dat[0]} | cut -d "/" -f1`
+					month=`echo ${dat[0]} | cut -d "/" -f2`
+					day=`echo ${dat[0]} | cut -d "/" -f3`
+					hour=`echo ${dat[0]} | cut -d "/" -f4`
+					fromy=`echo $3 | cut -d "/" -f1`
+					fromm=`echo $3 | cut -d "/" -f2`
+					fromd=`echo $3 | cut -d "/" -f3`
+					fromh=`echo $3 | cut -d "/" -f4`
+					toy=`echo $4 | cut -d "/" -f1`
+					tom=`echo $4 | cut -d "/" -f2`
+					tod=`echo $4 | cut -d "/" -f3`
+					toh=`echo $4 | cut -d "/" -f4`
+					time=`expr $year \* 8640 + $month \* 720 + $day \* 24 + $hour`
+					fromt=`expr $fromy \* 8640 + $fromm \* 720 + $fromd \* 24 + $fromh`
+					tot=`expr $toy \* 8640 + $tom \* 720 + $tod \* 24 + $toh`
+					if [ $time -ge $fromt -a $time -le $tot ]
+					then
+						echo ${dat[@]} >> ./stream/space.dat
+					fi
+				fi
+			done
+			./lib go
+			cp ./stream/output.dat ./result/space/date.dat
+		fi
+		cp space.dat ./stream/space.dat
+		cp input.dat ./stream/input.dat
 	fi
 elif [ $1 = 'output' ]
 then
